e7b6a2fb74a81e0ca42d477f18744bd8cd21cbcc
root <root@magma.localdomain>
2023-11-21 14:33:07 +0000

    add script, nginx confs

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install web server (Nginx)
+sudo apt update
+sudo apt install -y nginx
+
+# Configure firewall (allow SSH, HTTP, HTTPS)
+sudo ufw allow 22
+sudo ufw allow 80 #for site-a
+sudo ufw allow 443 #for site-a
+sudo ufw allow 8080 #for site-b
+sudo ufw enable
+
+# Add users
+# user 1
+# user 2
+
+#site-a
+sudo mkdir -p /var/www/site-a/html
+sudo chown -R $USER:$USER /var/www/site-a/html
+sudo chmod -R 755 /var/www/site-a
+
+#site-b
+sudo mkdir -p /var/www/site-b/html
+sudo chown -R $USER:$USER /var/www/site-b/html
+sudo chmod -R 755 /var/www/site-b
+
+sudo cat index > /var/www/site-a/html/index.html # дописать содержимое
+
+sudo cat def > /etc/nginx/sites-available/default # дописать содержимое
+
+# Add SSL certificate (Let's Encrypt)
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sudo certbot --nginx -d learndevopsandrei.ddns.net # get cert
+
+sudo cat addssl >> /etc/nginx/sites-available/default # add nginx config for ssl
+
+# Manage logs Nginx
