b7b8e5fd3993b1af93a81321f9c01b799e2a1526
Your Name <you@example.com>
2023-12-01 15:54:06 +0000

    compare script, edit sites path in nginx conf, add tee to sudoers

--- task-linux-scripting/script.sh
+++ task-linux-scripting/script.sh
@@ -23,7 +23,7 @@ while getopts ":n:p:s:" opt; do
   esac
 done
 
-# Install web server and php
+# Install web server
 sudo apt update
 sudo apt install -y nginx${vnginx} php-fpm${vphpfpm} openssl${vopenssl}
 
@@ -33,26 +33,62 @@ sudo ufw allow 80
 sudo ufw allow 443
 sudo ufw enable
 
-# Add users for sites administration, set ssh keys
-sudo useradd -m -s /bin/bash user-a
-sudo mkdir /home/user-a/.ssh
-sudo cp user-a-authorized_keys /home/user-a/.ssh/authorized_keys
-sudo useradd -m -s /bin/bash user-b
-sudo mkdir /home/user-b/.ssh
-sudo cp user-b-authorized_keys /home/user-a/.ssh/authorized_keys
+# Create chroot environment
+mkdir -p /chroot-jail-root/usr/bin
+# Copy utils to chroot
+cp /usr/bin/{bash,cat,ls,vi} /chroot-jail-root/usr/bin/
+
+# Copy utils library to chroot
+sudo cp -r lib /chroot-jail-root/usr/
+sudo ln -rs /chroot-jail-root/usr/lib /chroot-jail-root/lib
+sudo ln -rs /chroot-jail-root/usr/lib /chroot-jail-root/lib64
+
+# Provide basic configuration for chroot
+sudo mkdir -p /chroot-jail-root/etc /chroot-jail-root/usr/share
+sudo cp /etc/{group,passwd,shadow,hosts} /chroot-jail-root/etc
+sudo cp -r /usr/share/terminfo /chroot-jail-root/usr/share/
+
+# Provide the special files in directories in /dev for chroot
+sudo mkdir /chroot-jail-root/dev
+sudo mknod /chroot-jail-root/dev/null c 1 3
+sudo mknod /chroot-jail-root/dev/zero c 1 5
+sudo mknod /chroot-jail-root/dev/tty c 5 0
+sudo mknod /chroot-jail-root/dev/random c 1 8
+sudo mknod /chroot-jail-root/dev/urandom c 1 9
+sudo chmod 0666 /chroot-jail-root/dev/{null,tty,zero}
+sudo chown root:tty /chroot-jail-root/dev/tty
+
+# Create users and add ssk keys
+sudo mkdir -p /chroot-jail-root/home
+
+# Create user-a and configure ssh
+sudo useradd --base-dir /chroot-jail-root/home --create-home --shell /usr/bin/bash user-a
+sudo mkdir -p /chroot-jail-root/home/user-a/.ssh
+sudo cp user-a-authorized_keys /chroot-jail-root/home/user-a/.ssh/authorized_keys
+sudo chmod 600 /chroot-jail-root/home/user-a/.ssh/authorized_keys
+sudo chown -R user-a:user-a /chroot-jail-root/home/user-a/.ssh/
+
+# Create user-b and configure ssh
+sudo useradd --base-dir /chroot-jail-root/home --create-home --shell /usr/bin/bash user-b
+sudo mkdir -p /chroot-jail-root/home/user-b/.ssh
+sudo cp user-b-authorized_keys /chroot-jail-root/home/user-b/.ssh/authorized_keys
+sudo chmod 600 /chroot-jail-root/home/user-b/.ssh/authorized_keys
+sudo chown -R user-b:user-b /chroot-jail-root/home/user-b/.ssh/
+
+# Configure SSH to chroot
+sudo printf "Match User user-a,user-b\n\tChrootDirectory /chroot-jail-root\n\tPasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+sudo systemctl reload sshd.service
 
 #site-a
-sudo mkdir -p /var/www/site-a/html
-sudo chown -R user-a:user-a /var/www/site-a/html
-sudo chmod -R 755 /var/www/site-a
+sudo mkdir -p /chroot-jail-root/home/user-a/site-a/html
+sudo cp index.php /chroot-jail-root/home/user-a/site-a/html/index.php # create server info page for site-a
+sudo chown -R user-a:user-a /chroot-jail-root/home/user-a/site-a/html
 
 #site-b
-sudo mkdir -p /var/www/site-b/html
-sudo chown -R user-b:user-b /var/www/site-b/html
-sudo chmod -R 755 /var/www/site-b
+sudo mkdir -p /chroot-jail-root/home/user-b/site-a/html
+sudo cp /var/www/html/index.nginx-debian.html /chroot-jail-root/home/user-b/site-b/html/index.html # default server info page for site-b
+sudo chown -R user-b:user-b /chroot-jail-root/home/user-b/site-b/html
 
-sudo cp index.php /var/www/site-a/html/index.php # create server info page for site-a
-sudo cp /var/www/html/index.nginx-debian.html /var/www/site-b/html/index.html # default server info page for site-b
 sudo cp nginxconf /etc/nginx/sites-available/default # setting nginx config
 
 # Add SSL certificate (self-sign ssl)
