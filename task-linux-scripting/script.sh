ae23fe8fc51a1244962b71bf0e3b3138f532ca4f
Your Name <you@example.com>
2023-12-02 16:45:37 +0000

    add var to path, delete test

--- task-linux-scripting/script.sh
+++ task-linux-scripting/script.sh
@@ -34,60 +34,61 @@ sudo ufw allow 443
 sudo ufw enable
 
 # Create chroot environment
-mkdir -p /chroot-jail-root/usr/bin
+jailpath=/home/jail
+sudo mkdir -p $jailpath/usr/bin
 # Copy utils to chroot
-cp /usr/bin/{bash,cat,ls,vi} /chroot-jail-root/usr/bin/
+sudo cp /usr/bin/{bash,cat,ls,vi} $jailpath/usr/bin/
 
 # Copy utils library to chroot
-sudo cp -r lib /chroot-jail-root/usr/
-sudo ln -rs /chroot-jail-root/usr/lib /chroot-jail-root/lib
-sudo ln -rs /chroot-jail-root/usr/lib /chroot-jail-root/lib64
+sudo cp -r lib $jailpath/usr/
+sudo ln -rs $jailpath/usr/lib $jailpath/lib
+sudo ln -rs $jailpath/usr/lib $jailpath/lib64
 
 # Provide basic configuration for chroot
-sudo mkdir -p /chroot-jail-root/etc /chroot-jail-root/usr/share
-sudo cp /etc/{group,passwd,shadow,hosts} /chroot-jail-root/etc
-sudo cp -r /usr/share/terminfo /chroot-jail-root/usr/share/
+sudo mkdir -p $jailpath/etc $jailpath/usr/share
+sudo cp /etc/{group,passwd,shadow,hosts} $jailpath/etc
+sudo cp -r /usr/share/terminfo $jailpath/usr/share/
 
 # Provide the special files in directories in /dev for chroot
-sudo mkdir /chroot-jail-root/dev
-sudo mknod /chroot-jail-root/dev/null c 1 3
-sudo mknod /chroot-jail-root/dev/zero c 1 5
-sudo mknod /chroot-jail-root/dev/tty c 5 0
-sudo mknod /chroot-jail-root/dev/random c 1 8
-sudo mknod /chroot-jail-root/dev/urandom c 1 9
-sudo chmod 0666 /chroot-jail-root/dev/{null,tty,zero}
-sudo chown root:tty /chroot-jail-root/dev/tty
+sudo mkdir $jailpath/dev
+sudo mknod $jailpath/dev/null c 1 3
+sudo mknod $jailpath/dev/zero c 1 5
+sudo mknod $jailpath/dev/tty c 5 0
+sudo mknod $jailpath/dev/random c 1 8
+sudo mknod $jailpath/dev/urandom c 1 9
+sudo chmod 0666 $jailpath/dev/{null,tty,zero}
+sudo chown root:tty $jailpath/dev/tty
 
 # Create users and add ssk keys
-sudo mkdir -p /chroot-jail-root/home
+sudo mkdir -p $jailpath/home
 
 # Create user-a and configure ssh
-sudo useradd --base-dir /chroot-jail-root/home --create-home --shell /usr/bin/bash user-a
-sudo mkdir -p /chroot-jail-root/home/user-a/.ssh
-sudo cp user-a-authorized_keys /chroot-jail-root/home/user-a/.ssh/authorized_keys
-sudo chmod 600 /chroot-jail-root/home/user-a/.ssh/authorized_keys
-sudo chown -R user-a:user-a /chroot-jail-root/home/user-a/.ssh/
+sudo useradd --base-dir $jailpath/home --create-home --shell /usr/bin/bash user-a
+sudo mkdir -p $jailpath/home/user-a/.ssh
+sudo cp user-a-authorized_keys $jailpath/home/user-a/.ssh/authorized_keys
+sudo chmod 600 $jailpath/home/user-a/.ssh/authorized_keys
+sudo chown -R user-a:user-a $jailpath/home/user-a/.ssh/
 
 # Create user-b and configure ssh
-sudo useradd --base-dir /chroot-jail-root/home --create-home --shell /usr/bin/bash user-b
-sudo mkdir -p /chroot-jail-root/home/user-b/.ssh
-sudo cp user-b-authorized_keys /chroot-jail-root/home/user-b/.ssh/authorized_keys
-sudo chmod 600 /chroot-jail-root/home/user-b/.ssh/authorized_keys
-sudo chown -R user-b:user-b /chroot-jail-root/home/user-b/.ssh/
+sudo useradd --base-dir $jailpath/home --create-home --shell /usr/bin/bash user-b
+sudo mkdir -p $jailpath/home/user-b/.ssh
+sudo cp user-b-authorized_keys $jailpath/home/user-b/.ssh/authorized_keys
+sudo chmod 600 $jailpath/home/user-b/.ssh/authorized_keys
+sudo chown -R user-b:user-b $jailpath/home/user-b/.ssh/
 
 # Configure SSH to chroot
-sudo printf "Match User user-a,user-b\n\tChrootDirectory /chroot-jail-root\n\tPasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+printf "Match User user-a,user-b\n\tChrootDirectory $jailpath\n\tPasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config > /dev/null
 sudo systemctl reload sshd.service
 
 #site-a
-sudo mkdir -p /chroot-jail-root/home/user-a/site-a/html
-sudo cp index.php /chroot-jail-root/home/user-a/site-a/html/index.php # create server info page for site-a
-sudo chown -R user-a:user-a /chroot-jail-root/home/user-a/site-a/html
+sudo mkdir -p $jailpath/home/user-a/site-a/html
+sudo cp index.php $jailpath/home/user-a/site-a/html/index.php # create server info page for site-a
+sudo chown -R user-a:user-a $jailpath/home/user-a/site-a/html
 
 #site-b
-sudo mkdir -p /chroot-jail-root/home/user-b/site-a/html
-sudo cp /var/www/html/index.nginx-debian.html /chroot-jail-root/home/user-b/site-b/html/index.html # default server info page for site-b
-sudo chown -R user-b:user-b /chroot-jail-root/home/user-b/site-b/html
+sudo mkdir -p $jailpath/home/user-b/site-a/html
+sudo cp /var/www/html/index.nginx-debian.html $jailpath/home/user-b/site-b/html/index.html # default server info page for site-b
+sudo chown -R user-b:user-b $jailpath/home/user-b/site-b/html
 
 sudo cp nginxconf /etc/nginx/sites-available/default # setting nginx config
 
