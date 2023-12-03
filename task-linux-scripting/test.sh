c394d0e594123f31fa4fffe99c2c7e2e0212d0c5
Your Name <you@example.com>
2023-12-03 10:17:24 +0000

    delete test

--- task-linux-scripting/test.sh
+++
@@ -1,8 +0,0 @@
-# Manage restart php in chroot
-jailpath=/home/jail
-sudo touch /home/reload-php
-sudo cp /home/reload-php $jailpath/home/user-a/
-sudo mount --bind /home/reload-php $jailpath/home/user-a/reload-php 
-sudo chown user-a:user-a /home/reload-php
-sudo cp daemon.sh /home/
-sudo /home/daemon.sh &
