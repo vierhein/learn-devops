e499aa9e9318f6ed4c1a3430162cf057d62cfb0d
Your Name <you@example.com>
2023-12-13 16:03:20 +0000

    fix regexp

--- docker-tasks/task2/script.sh
+++ docker-tasks/task2/script.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
+version='119.0.6045.58'
+curl -L https://chromium.googlesource.com/chromium/src/+/refs/tags/${version} > tmp 
+position=$(cat tmp | grep Cr-Branched-From: | grep -oP '/heads/main@\{#\K[^}]+')
 
-version='118.0.5993.175'
-data=$(curl https://chromium.googlesource.com/chromium/src/+/refs/tags/${version}) 
-position=$(echo $data | grep Cr-Branched-From: | grep -oP '\{#(\d+)\}' | grep -oP '\d+')
 echo "this is $position"
 # uri=$(echo $data | grep -oP '(?<=parent).*?(?=diff)' | grep -oP '<a[^>]*href="\K[^"]*' | sed -n 1p)
 # url="https://chromium.googlesource.com${uri}"
 
 while [ -z "$position" ]; do
-    uri=$(echo $data | grep -oP '(?<=parent).*?(?=diff)' | grep -oP '<a[^>]*href="\K[^"]*' | sed -n 1p)
+    uri=$(cat tmp | grep -oP '(?<=parent).*?(?=diff)' | grep -oP '<a[^>]*href="\K[^"]*' | sed -n 1p)
     url="https://chromium.googlesource.com${uri}"
     echo $url
     echo "in while"
-    data=$(curl $url)
-    position=$(echo $data | grep Cr-Branched-From: | grep -oP '\{#(\d+)\}' | grep -oP '\d+')
+    curl -L $url > tmp
+    position=$(cat tmp | grep Cr-Branched-From: | grep -oP '/heads/main@\{#\K[^}]+')
     echo $position
-    sleep 5
 done
 
-# DOWNLOAD_LINK=$(jq -r ".linux64.\"${CHROMIUM_VERSION}\".download_url" chromium.stable.json) 
-# wget --no-check-certificate -O chromium.zip ${DOWNLOAD_LINK} 
+DOWNLOAD_LINK="https://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/${position}/chrome-linux.zip" 
+wget --no-check-certificate -O chromium.zip ${DOWNLOAD_LINK} 
 # unzip chromium.zip 
-# rm -f chromium.zip chromium.stable.json 
+# rm -f chromium.zip 
 # # Configure --no-sandbox and --ignore-certificate-errors
 # sed -i 's/\"$@\"/--no-sandbox --ignore-certificate-errors \"$@\"/' chrome-linux/chrome-wrapper 
 # ln -s /opt/chrome-linux/chrome-wrapper /usr/bin/chromium
