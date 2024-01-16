8e8231db9fe91ad7b372e2c801a2a92a2a02b752
Your Name <you@example.com>
2024-01-16 14:53:49 +0300

    edit backup module

--- terraform/task3/modules/backup-ec2/backups.tf
+++ terraform/task3/modules/backup-ec2/backups.tf
@@ -5,19 +5,19 @@ locals {
   }
 }
 
-resource "aws_backup_vault" "example-backup-vault" {
+resource "aws_backup_vault" "mongodb-backup-vault" {
   name = "mongodb-backup-vault"
   tags = {
     Role    = "backup-vault"
   }
 }
 
-resource "aws_backup_plan" "example-backup-plan" {
+resource "aws_backup_plan" "mongodb-backup-plan" {
   name = "mongodb-backup-plan"
 
   rule {
     rule_name         = "every-hour-${local.backups.retention}-day-retention"
-    target_vault_name = aws_backup_vault.example-backup-vault.name
+    target_vault_name = aws_backup_vault.mongodb-backup-vault.name
     schedule          = local.backups.schedule
     start_window      = 60
     completion_window = 120
@@ -37,10 +37,10 @@ resource "aws_backup_plan" "example-backup-plan" {
   }
 }
 
-resource "aws_backup_selection" "example-server-backup-selection" {
-  iam_role_arn = aws_iam_role.example-aws-backup-service-role.arn
+resource "aws_backup_selection" "mongodb-server-backup-selection" {
+  iam_role_arn = aws_iam_role.mongodb-aws-backup-service-role.arn
   name         = "mongo-server-resources"
-  plan_id      = aws_backup_plan.example-backup-plan.id
+  plan_id      = aws_backup_plan.mongodb-backup-plan.id
 
   selection_tag {
     type  = "STRINGEQUALS"
--- terraform/task3/modules/backup-ec2/iam.tf
+++ terraform/task3/modules/backup-ec2/iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "example-aws-backup-service-assume-role-policy-doc" {
+data "aws_iam_policy_document" "mongodb-aws-backup-service-assume-role-policy-doc" {
   statement {
     sid     = "AssumeServiceRole"
     actions = ["sts:AssumeRole"]
@@ -11,25 +11,23 @@ data "aws_iam_policy_document" "example-aws-backup-service-assume-role-policy-do
   }
 }
 
-/* The policies that allow the backup service to take backups and restores */
 data "aws_iam_policy" "aws-backup-service-policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
 }
 
 data "aws_caller_identity" "current_account" {}
 
-/* Roles for taking AWS Backups */
-resource "aws_iam_role" "example-aws-backup-service-role" {
-  name               = "ExampleAWSBackupServiceRole"
+resource "aws_iam_role" "mongodb-aws-backup-service-role" {
+  name               = "MongodbAWSBackupServiceRole"
   description        = "Allows the AWS Backup Service to take scheduled backups"
-  assume_role_policy = data.aws_iam_policy_document.example-aws-backup-service-assume-role-policy-doc.json
+  assume_role_policy = data.aws_iam_policy_document.mongodb-aws-backup-service-assume-role-policy-doc.json
 
   tags = {
     Role    = "iam"
   }
 }
 
-resource "aws_iam_role_policy" "example-backup-service-aws-backup-role-policy" {
+resource "aws_iam_role_policy" "mongodb-backup-service-aws-backup-role-policy" {
   policy = data.aws_iam_policy.aws-backup-service-policy.policy
-  role   = aws_iam_role.example-aws-backup-service-role.name
+  role   = aws_iam_role.mongodb-aws-backup-service-role.name
 }
