c8a8129576f48f5aa65ef57fe78edd9a6f35239d
Your Name <you@example.com>
2024-01-18 15:01:29 +0300

    delete unused, edit code style

--- terraform/task2/main.tf
+++ terraform/task2/main.tf
@@ -1,22 +1,6 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
-  }
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
 module "docker_containers" {
   source             = "./modules/docker-container"
   container_config   = var.container_config
   env                = var.env
 }
 
-output "container_info" {
-    value = "${module.docker_containers.container_info}"
-}
