c5cd13d55bd7470883b927fe0314f48e2c341e44
Your Name <you@example.com>
2024-02-05 22:59:24 +0300

    init

---
+++ aws/task2/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {}
+variable "aws_instance_count" {}
+variable "aws_ami_id" {}
+variable "aws_instance_type" {}
+variable "public_key_path" {}
