eb9a66848037f43248ecad0e157e6ee9b403c5c0
Your Name <you@example.com>
2024-02-08 18:31:11 +0300

    testing: get secrets work

--- aws/task2/iam.tf
+++ aws/task2/iam.tf
@@ -17,25 +17,6 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 EOF
 }
-resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs_task_role"
- 
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
  
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
