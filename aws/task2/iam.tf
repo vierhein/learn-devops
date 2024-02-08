09542f5bdf8da2849e20fdb935d73031757df5b6
Your Name <you@example.com>
2024-02-08 23:45:27 +0300

    add iam autoscale role

--- aws/task2/iam.tf
+++ aws/task2/iam.tf
@@ -27,3 +27,29 @@ resource "aws_iam_role_policy_attachment" "ecs_get_secret_role_policy_attachment
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
+
+# autoscale
+resource "aws_iam_role" "ecs_autoscale_role" {
+  name = "ecs_autoscale_role"
+ 
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "application-autoscaling.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_autoscale_role_policy_attachment" {
+  role       = aws_iam_role.ecs_autoscale_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+}
