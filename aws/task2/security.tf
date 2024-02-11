b093d2af897fce18a8f7e950b67f9894f8f7114a
Your Name <you@example.com>
2024-02-11 14:53:53 +0300

    edit sg

--- aws/task2/security.tf
+++ aws/task2/security.tf
@@ -1,22 +1,29 @@
-# Security Group for instances in public subnet
 resource "aws_security_group" "public_sg_one" {
   vpc_id = aws_vpc.vpc_one.id
 
-  ingress {
-        protocol    = "tcp"
-        from_port   = var.app_port
-        to_port     = var.app_port
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        protocol    = "-1"
-        from_port   = 0
-        to_port     = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
     Name = "public-sg-one"
   }
+}
+
+resource "aws_security_group_rule" "sg_one_allow_ssh" {
+  type              = "ingress"
+  from_port         = var.app_port
+  to_port           = var.app_port
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public_sg_one.id
+}
+
+resource "aws_security_group_rule" "sg_one_egress_allow_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public_sg_one.id
 }
