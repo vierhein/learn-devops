c007d10e17d09e84685b51e0e16b240b386225af
Your Name <you@example.com>
2024-02-11 15:03:54 +0300

    delete unused

--- aws/task2/variables.tf
+++ aws/task2/variables.tf
@@ -7,15 +7,12 @@ variable "public_subnet_one_cidr_block" {}
 variable "public_subnet_two_cidr_block" {}
 
 variable "app_image" {
-    description = "Docker image to run in the ECS cluster"
     default = "357197750522.dkr.ecr.eu-central-1.amazonaws.com/test:latest"
 }
 variable "app_port" {
-    description = "Port exposed by the docker image to redirect traffic to"
     default = 80
 }
 variable "app_desired" {
-    description = "Number of docker containers to run"
     default = 3
 }
 variable "fargate_cpu" {
