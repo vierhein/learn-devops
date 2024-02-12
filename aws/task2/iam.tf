70c53480d27e57b6efe4bcbeb6596d90c3bbc6c6
Your Name <you@example.com>
2024-02-13 00:04:14 +0300

    test policy

--- aws/task2/iam.tf
+++ aws/task2/iam.tf
@@ -53,3 +53,45 @@ resource "aws_iam_role_policy_attachment" "ecs_autoscale_role_policy_attachment"
   role       = aws_iam_role.ecs_autoscale_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
 }
+
+
+# data "aws_iam_policy_document" "assume_ecs_task_execution_role" {
+#   statement {
+#     effect = "Allow"
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
+
+# resource "aws_iam_role" "ecs_task_execution_role" {
+#   name               = "ecs_task_execution_role"
+#   assume_role_policy = data.aws_iam_policy_document.assume_ecs_task_execution_role.json
+# }
+
+# data "aws_iam_policy_document" "policy_task_execution_role" {
+#   statement {
+#     effect    = "Allow"
+#     actions   = [
+#         "ecr:GetAuthorizationToken",
+#         "ecr:BatchCheckLayerAvailability",
+#         "ecr:GetDownloadUrlForLayer",
+#         "ecr:BatchGetImage"
+#     ]
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "policy_task_execution_role" {
+#   name        = "policy_task_execution_role"
+#   policy      = data.aws_iam_policy_document.policy_task_execution_role.json
+# }
+
+# resource "aws_iam_role_policy_attachment" "test-attach" {
+#   role       = aws_iam_role.ecs_task_execution_role.name
+#   policy_arn = aws_iam_policy.policy_task_execution_role.arn
+# }
