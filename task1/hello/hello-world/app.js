94e41a12a2045dd23916d5080cae1afd6b4af24d
Your Name <you@example.com>
2024-03-13 19:40:12 +0300

    test4

--- task1/hello/hello-world/app.js
+++ task1/hello/hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello test4',
+                message: 'hello test4!!!',
                 // location: ret.data.trim()
             })
         }
